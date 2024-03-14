b2dafe03fc171dfe4de4e6a7189c050c1f44dd98
sreenath <sreenathbr20@gmail.com>
2024-03-14 17:30:49 +0530

    Added filter demo

--- src/components/MapDemo.js
+++ src/components/MapDemo.js
@@ -1,10 +1,12 @@
 import { peoples } from '../resources/data'
 import './MapDemo.css'
 
+
 function ProfileCard(props) {
     let people = props.people;
 
     let url = 'https://i.imgur.com/' + people.imageId + 's.jpg';
+
 
     return (
         <>
@@ -20,12 +22,14 @@ function ProfileCard(props) {
 
 }
 
-function ProfileTable(props) {
-    let employees = props.emp;
+// function ProfileTable(props) {
+//     let employees = props.employees;
+//     let profession = props.profession;
 
+function ProfileTable({ employees, profession }) {
     return (
         <>
-            <h3> Display all people in Table format:</h3>
+            <h3> Display {profession} people in Table format:</h3>
             <table>
                 <thead>
                     <tr>
@@ -36,7 +40,7 @@ function ProfileTable(props) {
                 </thead>
                 <tbody>
                     {
-                        employees.map((p) => <tr>
+                        employees.map((p) => <tr key={p.id}>
                             <td>{p.name}</td>
                             <td>{p.profession}</td>
                             <td>{p.accomplishment}</td>
@@ -53,7 +57,8 @@ function ProfileTable(props) {
 export default function MapDemo() {
 
     const fruits = ["Apple", "Grapes", "Guva"];
-
+    const chemists = peoples.filter((p) => p.profession === 'chemist');
+    const physicist = peoples.filter((p) => p.profession === 'physicist');
 
 
     return (
@@ -62,8 +67,12 @@ export default function MapDemo() {
                 {fruits.map((f, index) => <li key={index}>{f}</li>)}
             </ol>
 
+            <ProfileTable employees={peoples} profession='all'></ProfileTable>
 
-            <ProfileTable emp={peoples}></ProfileTable>
+            <ProfileTable employees={chemists} profession='chemist'></ProfileTable>
+
+            <ProfileTable employees={physicist} profession='physicist'></ProfileTable>
+
             <ProfileCard people={peoples[0]}></ProfileCard>
 
             <h6>List of all my People:</h6>
